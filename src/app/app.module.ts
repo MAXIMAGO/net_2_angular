edee7e90173c94dc0fdda5ccbf301e0a18820a90
kos <kos86@live.de>
2018-06-26 23:18:12 +0200

    layouted with angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProjectListComponent } from './components/project-list/project-list.component';
+import { MatButtonModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
+const MATERIAL = [MatToolbarModule, MatButtonModule, MatSidenavModule, BrowserAnimationsModule, MatListModule];
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProjectListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ProjectListComponent],
+  imports: [BrowserModule, MATERIAL],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
